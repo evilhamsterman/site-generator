61c887d20d52a88c23417c94d39b35b4dda2e24f
Dan Mills <evilhamsterman@gmail.com>
2024-05-02 05:07:07 +0000

    Handle other node types

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -37,36 +37,46 @@ def extract_markdown_links(text: str) -> list[tuple[str, str]]:
 def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     nodes = []
     for node in old_nodes:
-        if node.text == "":
+        if node.text_type != TextTypes.TEXT:
+            nodes.append(node)
             continue
         links = extract_markdown_images(node.text)
         text = node.text
+        if len(links) == 0:
+            nodes.append(node)
+            continue
         for link in links:
             text_parts = text.split(f"![{link[0]}]({link[1]})", 1)
-            nodes.extend(
-                [
-                    TextNode(text=text_parts[0], text_type=TextTypes.TEXT),
-                    TextNode(text=link[0], text_type=TextTypes.IMAGE, url=link[1]),
-                ]
-            )
+            if len(text_parts) != 2:
+                raise ValueError("Invalid Markdown, image not closed")
+            if text_parts[0] != "":
+                nodes.append(TextNode(text=text_parts[0], text_type=TextTypes.TEXT))
+            nodes.append(TextNode(text=link[0], text_type=TextTypes.IMAGE, url=link[1]))
             text = text_parts[1]
+        if text != "":
+            nodes.append(TextNode(text=text, text_type=TextTypes.TEXT))
     return nodes
 
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
     nodes = []
     for node in old_nodes:
-        if node.text == "":
+        if node.text_type != TextTypes.TEXT:
+            nodes.append(node)
             continue
         links = extract_markdown_links(node.text)
         text = node.text
+        if len(links) == 0:
+            nodes.append(node)
+            continue
         for link in links:
             text_parts = text.split(f"[{link[0]}]({link[1]})", 1)
-            nodes.extend(
-                [
-                    TextNode(text=text_parts[0], text_type=TextTypes.TEXT),
-                    TextNode(text=link[0], text_type=TextTypes.LINK, url=link[1]),
-                ]
-            )
+            if len(text_parts) != 2:
+                raise ValueError("Invalid Markdown, link not closed")
+            if text_parts[0] != "":
+                nodes.append(TextNode(text=text_parts[0], text_type=TextTypes.TEXT))
+            nodes.append(TextNode(text=link[0], text_type=TextTypes.LINK, url=link[1]))
             text = text_parts[1]
+        if text != "":
+            nodes.append(TextNode(text=text, text_type=TextTypes.TEXT))
     return nodes
