e1afce9a164c69873b8114f6b0c502b48767f97b
Dan Mills <evilhamsterman@gmail.com>
2024-05-11 00:29:12 +0000

    Block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,16 @@
+import re
+from enum import StrEnum
+
+
+class BlockTypes(StrEnum):
+    PARAGRAPH: str = "paragraph"
+    HEADING: str = "heading"
+    CODE: str = "code"
+    QUOTE: str = "quote"
+    UNORDERD_LIST: str = "unordered_list"
+    ORDERED_LIST: str = "orderd_list"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     lines = markdown.splitlines()
     blocks = []
@@ -14,3 +27,23 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     if block != "":
         blocks.append(block.rstrip("\n"))
     return blocks
+
+
+def block_to_block_type(markdown: str) -> BlockTypes:
+    lines = markdown.splitlines()
+    if len(lines) == 1 and re.match(r"[#]+ [\S]+", lines[0]):
+        return BlockTypes.HEADING
+    if lines[0] == "```" and lines[-1] == "```":
+        return BlockTypes.CODE
+    if all(map(lambda x: re.match(r"^>", x), lines)):
+        return BlockTypes.QUOTE
+    if all(map(lambda x: re.match(r"^[*|-] ", x), lines)):
+        return BlockTypes.UNORDERD_LIST
+    ordered_list = True
+    for i in range(1, len(lines)):
+        if not re.match(f"^{i}. ", lines[i - 1]):
+            ordered_list = False
+            break
+    if ordered_list:
+        return BlockTypes.ORDERED_LIST
+    return BlockTypes.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,18 +1,22 @@
 import unittest
+from textwrap import dedent
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockTypes, block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        markdown = """This is **bolded** paragraph
+        markdown = dedent(
+            """
+            This is **bolded** paragraph
 
-This is another paragraph with *italic* text and `code` here
-This is the same paragraph on a new line
+            This is another paragraph with *italic* text and `code` here
+            This is the same paragraph on a new line
 
-* This is a list
-* with items
-"""
+            * This is a list
+            * with items
+            """
+        )
         expected = [
             "This is **bolded** paragraph",
             "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",  # noqa: E501
@@ -22,18 +26,20 @@ This is the same paragraph on a new line
         self.assertListEqual(expected, actual)
 
     def test_markdown_to_blocks_newlines(self):
-        markdown = """
-This is **bolded** paragraph
+        markdown = dedent(
+            """
+            This is **bolded** paragraph
 
 
 
 
-This is another paragraph with *italic* text and `code` here
-This is the same paragraph on a new line
+            This is another paragraph with *italic* text and `code` here
+            This is the same paragraph on a new line
 
-* This is a list
-* with items
-"""
+            * This is a list
+            * with items
+            """
+        )
         expected = [
             "This is **bolded** paragraph",
             "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",  # noqa: E501
@@ -41,6 +47,141 @@ This is the same paragraph on a new line
         ]
         actual = markdown_to_blocks(markdown)
         self.assertListEqual(expected, actual)
+
+    def test_block_to_block_type_heading(self):
+        block = "# heading"
+        expected = BlockTypes.HEADING
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_heading_invalid(self):
+        block = dedent(
+            """
+            # heading
+            not heading
+        """
+        )
+        expected = BlockTypes.PARAGRAPH
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_code(self):
+        block = dedent(
+            """\
+            ```
+            1+2
+            ```
+            """
+        )
+        expected = BlockTypes.CODE
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_code_invalid(self):
+        block = dedent(
+            """\
+            ```
+            1+2
+            """
+        )
+        expected = BlockTypes.PARAGRAPH
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_quote(self):
+        block = dedent(
+            """\
+            > this
+            > is
+            > quotes\
+            """
+        )
+        expected = BlockTypes.QUOTE
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_quote_invalid(self):
+        block = dedent(
+            """\
+            > this
+            > is
+            > quotes
+
+            """
+        )
+        expected = BlockTypes.PARAGRAPH
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_unordered_list(self):
+        blocks = [
+            dedent(
+                """\
+            * list
+            * with
+            * stars
+            """
+            ),
+            dedent(
+                """\
+            - list
+            - with
+            - lines
+            """
+            ),
+        ]
+        expected = BlockTypes.UNORDERD_LIST
+        for list_type in blocks:
+            actual = block_to_block_type(list_type)
+            self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_unordered_list_invalid(self):
+        blocks = [
+            dedent(
+                """\
+            * list
+            * with
+            * stars
+            not
+            """
+            ),
+            dedent(
+                """\
+            - list
+            - with
+            - lines
+            not
+            """
+            ),
+        ]
+        expected = BlockTypes.PARAGRAPH
+        for list_type in blocks:
+            actual = block_to_block_type(list_type)
+            self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = dedent(
+            """\
+            1. this
+            2. is
+            3. list
+            """
+        )
+        expected = BlockTypes.ORDERED_LIST
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_ordered_list_invalid(self):
+        block = dedent(
+            """\
+            1. this
+            4. is
+            3. list
+            """
+        )
+        expected = BlockTypes.PARAGRAPH
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == "__main__":
